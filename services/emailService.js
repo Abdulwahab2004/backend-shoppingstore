70ba1b6f37a3ba32782abc675406a52ed1fe33fb
Abdul Wahab Nadeem <wahabndm@gmail.com>
2026-07-20 12:51:34 +0500

    try port 587 for smtp

--- services/emailService.js
+++ services/emailService.js
@@ -2,7 +2,9 @@ const nodemailer = require("nodemailer");
 
 const sendVerificationEmail = async (toEmail, token) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // TLS, not SSL
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,

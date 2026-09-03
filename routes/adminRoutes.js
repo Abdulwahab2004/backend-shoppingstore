580219066b99f1d6d9113c8a39b8e6cebbb16e3f
Abdul Wahab Nadeem <wahabndm@gmail.com>
2026-09-03 16:56:03 +0500

    fixed route for admin order

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { getDashboardStats } = require("../controllers/adminController");
 const { getUsers, updateUserRole, deleteUser } = require("../controllers/userController");
+const { getAllOrders, updateOrderStatus, getOrderByIdAdmin } = require("../controllers/orderController");
 const protect = require("../middleware/authMiddleware");
 const admin = require("../middleware/adminMiddleware");
-const { getAllOrders, updateOrderStatus } = require("../controllers/orderController");
 
 router.use(protect, admin);
 
@@ -13,7 +13,9 @@ router.get("/stats", getDashboardStats);
 router.get("/users", getUsers);
 router.put("/users/:id/role", updateUserRole);
 router.delete("/users/:id", deleteUser);
+
 router.get("/orders", getAllOrders);
 router.put("/orders/:id/status", updateOrderStatus);
+router.get("/orders/:id", getOrderByIdAdmin);
 
 module.exports = router;
